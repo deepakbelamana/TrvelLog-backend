111262beed7a876ed4c6ad36590ce9e9b2453fa1
Deepak Belamana <belamana.deepak@lntinfotech.com>
2022-06-05 21:27:54 +0530

    login validation added

--- loginValidator.py
+++ loginValidator.py
@@ -1,11 +1,24 @@
-from flask import Blueprint
+import json
+from flask import Blueprint, jsonify,request
 from flask_cors import cross_origin
 from mongodbConnection import connectToDb
 loginValidator=Blueprint('loginValidator',__name__)
 
 database=connectToDb()
 
-@loginValidator.route('/app/login')
+@loginValidator.route('/app/login',methods=['post'])
 @cross_origin()
 def loginUser():
-    return 'Login Here'
+    form_data=request.data
+    collection=database.users
+    form_data=form_data.decode()
+    form_data=json.loads(form_data)
+    userExists=True if collection.count_documents({'userEmail':form_data['userEmail']})!=0 else False
+    if userExists:
+        user_data=collection.find_one({'userEmail':form_data['userEmail']})
+        if(user_data['userPassword']== form_data['userPassword']):
+            return jsonify(result=True,register=True,password=True)
+        else:
+            return jsonify(result=True,register=True,password=False)
+    else:
+        return jsonify(register=False,result=False,password=False)
--- mongodbConnection.py
+++ mongodbConnection.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 
 def connectToDb():
     try:   
-        cluster=MongoClient('mongodb+srv://travellog:travellog@travellog.t6ldjf8.mongodb.net/?retryWrites=true&w=majority')
+        cluster=MongoClient('mongodb://travellog:travellog@ac-9jri8uh-shard-00-00.t6ldjf8.mongodb.net:27017,ac-9jri8uh-shard-00-01.t6ldjf8.mongodb.net:27017,ac-9jri8uh-shard-00-02.t6ldjf8.mongodb.net:27017/?ssl=true&replicaSet=atlas-5h4dl2-shard-0&authSource=admin&retryWrites=true&w=majority')
         db=cluster.travellog
         return db
     except Exception as e :
